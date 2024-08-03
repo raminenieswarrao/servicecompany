4172cee8a6480a205450b9d901a0452e87f27427
Eswar <raminenieswarrao@gmmail.com>
2024-08-03 10:11:12 -0400

    pages where added

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Service Company</h1>
+      <nav>
+        <a href="/">Home</a> | <a href="/About">About</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Information about the service company.</p>
+    </div>
+  );
+};
+
+export default About;
